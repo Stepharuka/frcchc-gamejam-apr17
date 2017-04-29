498ddf68927875f16ec888cf5727fed1e371cb02
Stepharuka <stepharuka@gmail.com>
2017-04-29 15:25:29 -0600

    remove debug messages and unneeded functions

--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -10,59 +10,13 @@ onready var enemies_node = get_node("Level 1/Enemy Units")
 func _ready():
 	# set_process(true)
 	set_process_input(true)
-	print(players_node.get_used_cells())
 
 func _input(event):
 	#if event.type == InputEvent.MOUSE_MOTION: # Set mouse cursor.
 		#set_ui_cursor(ui_map.world_to_map(event.pos))
 	if (event.type == InputEvent.MOUSE_BUTTON and not event.is_pressed()):
 		var target_tile = terrain_map.world_to_map(event.pos)
-		print(terrain_map.map_to_world(target_tile))
 		var move_pos = terrain_map.map_to_world(target_tile)
 		if terrain_map.is_passable(target_tile):
 			pass
 			players_node.move_unit(Vector2(players_node.get_used_cells()[0]),target_tile)
-
-func calculate_tilemap_size(tilemap):
-    # Get list of all positions where there is a tile
-    var used_cells = tilemap.get_used_cells()
-
-    # If there are none, return null result
-    if used_cells.size() == 0:
-        return {x=0, y=0, width=0, height=0}
-
-    # Take first cell as reference
-    var min_x = used_cells[0].x
-    var min_y = used_cells[0].y
-    var max_x = min_x
-    var max_y = min_y
-
-    # Find bounds
-    for i in range(1, used_cells.size()):
-
-        var pos = used_cells[i]
-
-        if pos.x < min_x:
-            min_x = pos.x
-        elif pos.x > max_x:
-            max_x = pos.x
-
-        if pos.y < min_y:
-            min_y = pos.y
-        elif pos.y > max_y:
-            max_y = pos.y
-
-    # Return resulting bounds
-    return {
-        "x_min": min_x,
-        "y_min": min_y,
-        "x_max": max_x - min_x + 1,
-        "y_max": max_y - min_y + 1
-    }
-
-
-func is_valid_map_pos(tilemap_dims, pos):
-	if (pos.x > (tilemap_dims["x_min"]*TILE_SIZE) and pos.x < ((tilemap_dims["x_max"]*TILE_SIZE)-TILE_SIZE)): # x-cord bounds (based on map)
-		if (pos.y > (tilemap_dims["y_min"]*TILE_SIZE) and pos.y < ((tilemap_dims["y_max"]*TILE_SIZE)-TILE_SIZE)): # y-cord bounds (based on map)
-			return true
-	return false
